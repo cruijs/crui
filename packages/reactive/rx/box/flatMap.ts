a12337bce08abe64f01f24f9dfadcd77d9446679
spell <pelligra.s@gmail.com>
2019-06-08 23:29:50 +0200

    fix name: concat -> flatMap

--- packages/reactive/rx/box/flatMap.ts
+++ packages/reactive/rx/box/flatMap.ts
@@ -1,6 +1,6 @@
 import { StreamBox } from './index';
 
-export function concat<A, B>(box: StreamBox<A>, f: (b: A) => StreamBox<B>): StreamBox<B> {
+export function flatMap<A, B>(box: StreamBox<A>, f: (b: A) => StreamBox<B>): StreamBox<B> {
     let br = f(box.get())
     const z = new StreamBox(br.get())
     br.subscribe(z.set)

8404019d06c98bb740a220e7408c47c06838913b
spell <pelligra.s@gmail.com>
2019-06-15 16:12:31 +0200

    remove h$b use

--- packages/eg-reactive-todos/src/add-todo/index.ts
+++ packages/eg-reactive-todos/src/add-todo/index.ts
@@ -1,22 +1,18 @@
 import { onClick } from '@crui/core/combinators/events';
 import { hc } from '@crui/core/elems/children';
 import { text } from '@crui/core/elems/text';
-import { h, hss } from '@crui/css-emotion';
+import { hss } from '@crui/css-emotion';
 import { $bindVal } from '@crui/reactive/combinators/$bind';
 import { cloneRW } from '@crui/reactive/rx/box/clone';
+import { hcs } from '../../../css-emotion/elems/hcs';
 import { TodoStore } from '../store';
 
 const input = hss('input', {
     width: '13rem'
 })
-const submit = h('button', {
-    css: {
-        marginLeft: '0.5rem',
-    },
-    children: [
-        text('Add')
-    ],
-})
+const submit = hcs('button', { marginLeft: '0.5rem' }, [
+    text('Add')
+])
 export function AddTodo(store: TodoStore) {
     const inp = store.getInput()
     return hc('div', [
--- packages/reactive/index.ts
+++ packages/reactive/index.ts
@@ -1,5 +1,5 @@
 export { h$ } from './elems';
-export { h$b } from './elems/$bind';
+export { h$bv, h$bc } from './elems/$bind';
 export { h$p } from './elems/$props';
 export { h$map } from './elems/h$map';
 export { $ite } from './elems/ifThenElse';

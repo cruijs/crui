d99ee9a01caef9750eaa1e2f89dac45efa6f3a7f
spell <pelligra.s@gmail.com>
2019-06-22 01:48:02 +0200

    last fix

--- packages/eg-reactive-todos/src/add-todo/index.ts
+++ packages/eg-reactive-todos/src/add-todo/index.ts
@@ -1,5 +1,7 @@
 import { hc } from '@crui/core/elems/children';
 import { h } from '@crui/core/elems/h';
+import { text } from '@crui/core/elems/text';
+import { children } from '@crui/core/setups/children';
 import { onClick } from '@crui/core/setups/events';
 import { css } from '@crui/css-emotion/setups/css';
 import { cloneRW } from '@crui/reactive/rx/box/clone';
@@ -20,7 +22,10 @@ export function AddTodo(store: TodoStore) {
 
                 store.addTodo(inp.get())
                 inp.set('')
-            })
+            }),
+            children([
+                text('Add')
+            ])
         ])
     ])
 }

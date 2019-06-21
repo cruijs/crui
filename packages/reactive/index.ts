8ab03404ce32428e8e079fd3be3bc055924b678c
spell <pelligra.s@gmail.com>
2019-06-21 18:22:35 +0200

    $map

--- packages/reactive/index.ts
+++ packages/reactive/index.ts
@@ -1,5 +1,5 @@
 export * from './setups/$bind';
-export * from './elems/$map';
+export * from './setups/$map';
 export * from './elems/$props';
 export * from './setups/ifThenElse';
 export * from './elems/t$';
---
+++ packages/reactive/setups/$map.ts
@@ -0,0 +1,22 @@
+import { Component, Setup } from '@crui/core/dom';
+import { result } from '@crui/core/dom/rendered';
+import { R$L } from '../rx';
+import { $map as l$map } from '../rx/list/map';
+import { with$Children } from './internals/$children';
+
+/**
+ * Map a stream of items I into children and setup them in an element
+ */
+export function $map<T, C, M>(
+    $list: R$L<T>,
+    item: (i: T) => Component<C>
+): Setup<C, M> {
+    return (meta, dom, node, ctxt) => result(meta, with$Children(
+        dom,
+        node,
+        l$map(
+            $list,
+            (todo: T) => item(todo)(dom, ctxt)
+        )
+    ))
+}

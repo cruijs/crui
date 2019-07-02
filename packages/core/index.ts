f595566ae836c0f12425cec208321ce454312bad
spell <pelligra.s@gmail.com>
2019-07-02 18:22:43 +0200

    add some more export

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,4 +1,5 @@
 export { Component, Setup } from './dom';
+export { Meta } from './dom/rendered';
 export * from './elems';
 export * from './setups';
 export * from './types';
--- packages/core/setups/index.ts
+++ packages/core/setups/index.ts
@@ -1,5 +1,6 @@
 export * from './children';
 export * from './cleanup';
+export * from './combine';
 export * from './events';
 export * from './props';
 export * from './style';

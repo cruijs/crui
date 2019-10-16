9bad69fdca47f72dafbc382b382ca1745865b844
spell <pelligra.s@gmail.com>
2019-10-16 19:07:25 +0200

    css emotion

---
+++ packages/eg-reactive-todos/v2/actions/css/index.ts
@@ -0,0 +1,20 @@
+import { Interpolation } from 'emotion'
+import { Action, AnyAction, Driver } from '../../types'
+import { action } from '../action'
+import { TagR } from '../tagAction'
+
+export const CssType = Symbol('css')
+export type CssDriver<N = any, S extends AnyAction = AnyAction> = {
+    [CssType]: Driver<N, Css, S>
+}
+export type Style<S> = Interpolation<S>
+export type Css<S = undefined> = Action<typeof CssType, CssDriver, TagR> & {
+    style: Style<S>
+}
+
+export function css<S>(style: Style<S>): Css<S> {
+    return action({
+        type: CssType,
+        style,
+    })
+}

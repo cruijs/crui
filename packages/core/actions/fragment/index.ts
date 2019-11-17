005305c6e8478a42d9fd9bc696103de4a57743f3
spell <pelligra.s@gmail.com>
2019-11-17 01:09:08 +0100

    add fragment and insertAtRef to core index

--- packages/core/actions/fragment/index.ts
+++ packages/core/actions/fragment/index.ts
@@ -1,23 +1,3 @@
-import { Action, InfraAction, Driver } from '../../types'
-import { action } from '../action'
-
-export const FragmentType = Symbol('fragment')
-export type Fragment<N, F> = InfraAction<
-    typeof FragmentType,
-    FragmentDriver<N, F>,
-    {},
-    F
-> & {
-    children: N[]
-}
-
-export type FragmentDriver<N = any, F = any, S extends Action = never> = {
-    [FragmentType]: Driver<N, Fragment<N, F>, S, void, F>
-}
-
-export function fragment<N, F = N>(children: N[]): Fragment<N, F> {
-    return action({
-        type: FragmentType,
-        children,
-    })
-}
+export * from './action'
+export * from './driver-browser'
+export * from './driver-mock'
